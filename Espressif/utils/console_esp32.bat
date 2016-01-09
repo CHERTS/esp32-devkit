5c4cde334dfee07e4e68ff08747b70f167eb4969
Mikhail Grigorev <sleuthhound@gmail.com>
2016-01-09 23:32:40 +0500

    * Utils moved to a subfolder utils\ESP32;
    Signed-off-by: Mikhail Grigorev <sleuthhound@gmail.com>

--- Espressif/utils/console_esp32.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Setting environment for using Unofficial Development Kit for Espressif ESP32 tools.
-
-@call udkvars_esp32.bat
-%comspec%
--- Espressif/utils/udkvars_esp32.bat
+++
@@ -1,6 +0,0 @@
-@echo Setting environment for using Unofficial Development Kit for Espressif ESP32 tools.
-
-@set mingw_dir=C:\MinGW
-@set devkit_dir=C:\Espressif
-@set PATH=%devkit_dir%\xtensa-esp108-elf\bin;%mingw_dir%\bin;%mingw_dir%\msys\1.0\bin;%PATH%
-@cd %devkit_dir%\examples
