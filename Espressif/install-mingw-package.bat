8721bc38a01e3c8891dd733dc066678d179866dc
Mikhail Grigorev <sleuthhound@gmail.com>
2016-10-01 21:59:08 +0500

    Updated mingw script
    Signed-off-by: Mikhail Grigorev <sleuthhound@gmail.com>

--- Espressif/install-mingw-package.bat
+++ Espressif/install-mingw-package.bat
@@ -1,8 +1,44 @@
 @echo off
-
-title Installing MinGW packages...
+title Installing additional packages for MinGW
+cls
+echo.
+echo Installing additional packages for MinGW
+echo.
+net session >nul 2>&1
+if not %ERRORLEVEL% EQU 0 (
+      echo Error: To install packages need admin rights.
+      echo.
+      echo Run the script as an administrator.
+      echo.
+      echo To run as administrator, click on the file install.cmd the right mouse
+      echo button and select 'Run as administrator'
+      echo.
+      echo Press Enter to exit.
+      pause >nul
+      goto :eof
+)
+echo Press Ctrl-C to cancel, Enter to continue
+pause >nul
+cd /d %~dp0
+echo.
 
 if exist "C:\MinGW" (
+  if exist "C:\MinGW\var\cache\mingw-get" (
+    if not exist "mingw-cache-packages.zip" (
+      utils\ESP8266\wget.exe http://dl.programs74.ru/get.php?file=mingw-cache-packages -O mingw-cache-packages.zip
+    )
+    if exist "mingw-cache-packages.zip" (
+      copy /Y mingw-cache-packages.zip "C:\MinGW\var\cache\mingw-get"
+      copy /Y utils\ESP8266\unzip.exe "C:\MinGW\var\cache\mingw-get"
+      C:
+      cd C:\MinGW\var\cache\mingw-get
+      if exist "C:\MinGW\var\cache\mingw-get\mingw-cache-packages.zip" (
+        unzip.exe -o mingw-cache-packages.zip
+        del /Q /F mingw-cache-packages.zip
+        del /Q /F unzip.exe
+      )
+    )
+  )
   C:
   cd C:\MinGW\bin
   if exist mingw-get.exe (
@@ -18,6 +54,8 @@ if exist "C:\MinGW" (
     mingw-get install gcc-c++
     mingw-get install libz
     mingw-get install bzip2
+    mingw-get install gettext
+    mingw-get install pthreads-w32
     mingw-get install msys-base
     mingw-get install msys-coreutils
     mingw-get install msys-coreutils-ext
@@ -28,17 +66,33 @@ if exist "C:\MinGW" (
     mingw-get install msys-flex-bin
     mingw-get install msys-gawk
     mingw-get install msys-sed
+    mingw-get install msys-patch
     mingw-get install msys-autoconf
     mingw-get install msys-automake
     mingw-get install msys-mktemp
-    mingw-get install msys-patch
     mingw-get install msys-libtool
-    echo Installing modules for MinGW completed.
+    mingw-get install msys-help2man
+    mingw-get install msys-gettext
+    mingw-get install msys-perl
+    echo.
+    echo Installing additional packages for MinGW complete.
+    echo.
+    echo Press Enter to exit the installation wizard.
+    echo.
+    pause >nul
   ) else (
+    echo.
     echo ERROR! mingw-get.exe not found.
+    echo.
+    echo Press Enter to exit.
+    echo.
+    pause >nul
+    goto :eof
   )
 ) else (
+  echo.
   echo ERROR! MinGW not found.
+  echo.
+  echo Press Enter to exit.
+  echo.
 )
-pause
-
